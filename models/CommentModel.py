b3803ad3a84f07e0bf5c595f2b806b1a8f7a30ab
Mateusz Jankowski <mateusz573@gmail.com>
2025-02-03 16:08:11 +0100

    fixed CommentModel types

--- models/CommentModel.py
+++ models/CommentModel.py
@@ -1,19 +1,19 @@
-import datetime
+from datetime import datetime
 from models.model import Model
 from pydantic import BaseModel
 
 class Comment(BaseModel, Model):
-    commentID: str
-    reviewID: str
-    authorID: str
+    commentID: int
+    reviewID: int
+    authorID: int
     content: str
     timestamp: datetime
-    def __init__(self, commentID, reviewID, authorID, content, timestamp=datetime.datetime.today()):
+    def __init__(self, commentID, reviewID, authorID, content, timestamp=datetime.now()):
         self.commentID: int = commentID
         self.reviewID: int = reviewID
         self.authorID: int = authorID
         self.content: str = content
-        self.timestamp: datetime.date = timestamp
+        self.timestamp: datetime = timestamp
 
     def __str__(self):
         return str(self.authorID) + " (" + str(self.timestamp) + ") " + ": " + self.content
@@ -32,7 +32,7 @@ class Comment(BaseModel, Model):
                 reviewID=row["reviewID"],
                 authorID=row["authorID"],
                 content=row["content"],
-                timestamp=datetime.datetime.strptime(row["timestamp"], "%Y-%m-%d")
+                timestamp=datetime.strptime(row["timestamp"], "%Y-%m-%d")
             ))
 
         return comments
